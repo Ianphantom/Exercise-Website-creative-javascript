5ecc4c8e164338d96a2bd1592a7c331fb0e44b75
tugasSBD <ian25yola@gmail.com>
2021-08-15 01:03:44 +0700

    Generate Color

---
+++ Color Picker/app.js
@@ -0,0 +1,21 @@
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColor;
+
+function generateHex(){
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors(){
+    colorDivs.forEach((div, index) =>{
+        const hexText = div.children[0];
+        const randomColors = generateHex();
+        div.style.backgroundColor = randomColors;
+        hexText.innerText = randomColors;
+    });
+}
+
+randomColors();
