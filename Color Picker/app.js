05c8870371ac04b1da4ca1668d426306094307f1
tugasSBD <ian25yola@gmail.com>
2021-08-15 01:51:25 +0700

    Coloring my inputs

--- Color Picker/app.js
+++ Color Picker/app.js
@@ -15,7 +15,14 @@ function randomColors(){
         const randomColors = generateHex();
         div.style.backgroundColor = randomColors;
         hexText.innerText = randomColors;
-        checkTextContrast(randomColors, hexText)
+        checkTextContrast(randomColors, hexText);
+        const color = chroma(randomColors);
+        const sliders = div.querySelectorAll('.sliders input')
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
 }
 
@@ -26,6 +33,18 @@ function checkTextContrast(color,text){
     }else{
         text.style.color = "white"
     }
+}
+
+function colorizeSliders(color, hue, brightness, saturation){
+    const noSat = color.set('hsl.s',0);
+    const fullSat = color.set('hsl.s',1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)} ,${scaleBright(1)})`;
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`
 }
 
 randomColors();
