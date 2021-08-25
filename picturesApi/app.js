207af84a201019ec8c302b85b64030b885d05589
tugasSBD <ian25yola@gmail.com>
2021-08-25 18:01:06 +0700

    Refactoring my code

--- picturesApi/app.js
+++ picturesApi/app.js
@@ -3,7 +3,7 @@ const auth = "563492ad6f91700001000001f1368adb8ad047fc8ba2d81457b0add2";
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
-
+const home = `https://api.pexels.com/v1/curated?per_page=15&page=1`;
 let searchValue;
 
 
@@ -17,8 +17,8 @@ function updateInput(e){
     searchValue = e.target.value;
 }
 
-async function curatedPhotos(){
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+async function fetchApi(url){
+    const dataFetch = await fetch(url, {
         method : "GET",
         headers: {
             Accept : 'aplication/json',
@@ -26,6 +26,10 @@ async function curatedPhotos(){
         }
     });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo =>{
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
@@ -37,24 +41,16 @@ async function curatedPhotos(){
     }); 
 }
 
+async function curatedPhotos(){
+    data = await fetchApi(home);
+    generatePictures(data);
+}
+
 async function searchPhotos(query){
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`, {
-        method : "GET",
-        headers: {
-            Accept : 'aplication/json',
-            Authorization : auth
-        }
-    });
-    const data = await dataFetch.json();
-    data.photos.forEach(photo =>{
-        const galleryImg = document.createElement('div');
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `
-            <img src="${photo.src.large}"></img>
-            <p>${photo.photographer}</p>
-        `;
-        gallery.appendChild(galleryImg);
-    }); 
+    let url = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+    const data = await fetchApi(url);
+    generatePictures(data);
+
 }
 
 curatedPhotos();
